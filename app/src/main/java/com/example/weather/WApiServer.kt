8d3435eee946281ff24a927a457ba797177c339a
mohammedknan <162056238+mohammedknan@users.noreply.github.com>
2025-03-06 10:30:12 +0300

    Update WApiServer.kt

--- app/src/main/java/com/example/weather/WApiServer.kt
+++ app/src/main/java/com/example/weather/WApiServer.kt
@@ -13,35 +13,3 @@ interface WApiServer {
     ): Call<WeatherResponse>
 }
 
-
-
-//import retrofit2.Call
-//import retrofit2.http.GET
-//
-//
-//interface WApiServer {
-//@GET("comments")
-//fun getComments(
-////    @Query("q") location:String,
-////    @Query("appied") token:String
-//
-//):Call<List<WeatherResponse>>
-//
-//}
-
-
-
-//import retrofit2.Call
-//import retrofit2.http.GET
-//import retrofit2.http.Query
-//
-//
-//
-//interface WApiServer {
-//    @GET("current.json?key=c7cb884a7bec4222b87225029250302")
-//    fun getWeather(@Query("q") city: String): Call<WeatherResponse>
-//}
-
-
-
-
